d3360e2a3a39544dea9f3bcb72bf33fdbaf46021
salo <cba@salo.ai>
2020-05-04 10:25:49 -0700

    init

--- setup.py
+++ setup.py
@@ -1,36 +1,21 @@
-from setuptools import setup, find_packages
-exec(open('ccb/__version__.py').read())
+from setuptools import find_packages, setup
+
+__version__ = open("ccb/__version__.py").read()
 
 setup_args = {
-    'name': 'ccb',
-    'version': __version__,
-    'url': 'https://github.com/earth-chris/ccb',
-    'license': 'MIT',
-    'author': 'Christopher Anderson',
-    'author_email': 'cbanders@stanford.edu',
-    'description': 'Species distribution modeling support tools',
-    'keywords': [
-        'maxent',
-        'biogeography',
-        'SDM',
-        'species distribution modeling',
-        'ecologyy',
-        'conservation'
-    ],
-    'packages': ['ccb'],
-    'include_package_data': True,
-    'platforms': 'any',
-    'scripts': [
-        'bin/gbif-to-vector.py',
-        'bin/maxent-to-vector.py'
-    ],
-    'data_files': [
-        ('maxent', [
-            'ccb/maxent/maxent.jar',
-            'ccb/maxent/README.txt',
-            'ccb/maxent/LICENSE.txt'
-        ])
-    ]
+    "name": "ccb",
+    "version": __version__,
+    "url": "https://github.com/earth-chris/ccb",
+    "license": "MIT",
+    "author": "Christopher Anderson",
+    "author_email": "cbanders@stanford.edu",
+    "description": "Species distribution modeling support tools",
+    "keywords": ["maxent", "biogeography", "SDM", "species distribution modeling", "ecologyy", "conservation"],
+    "packages": ["ccb"],
+    "include_package_data": True,
+    "platforms": "any",
+    "scripts": ["bin/gbif-to-vector.py", "bin/maxent-to-vector.py"],
+    "data_files": [("maxent", ["ccb/maxent/maxent.jar", "ccb/maxent/README.txt", "ccb/maxent/LICENSE.txt"])],
 }
 
 setup(**setup_args)
