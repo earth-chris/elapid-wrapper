cbaae7e817a359eb17bbcc8cec9ef9338ff93997
earth-chris <cba@salo.ai>
2021-01-16 17:17:07 -0800

    added long description, code formatted

--- setup.py
+++ setup.py
@@ -1,21 +1,45 @@
 from setuptools import setup
 
-version = open("ccb/__version__.py").read().strip('"\n')
+version = open("elapid/__version__.py").read().strip('"\n')
+long_description = open("README.md", "r", encoding="utf-8").read()
 
 setup_args = {
-    "name": "ccb",
+    "name": "elapid",
     "version": version,
-    "url": "https://github.com/earth-chris/ccb",
+    "url": "https://github.com/earth-chris/elapid",
     "license": "MIT",
     "author": "Christopher Anderson",
     "author_email": "cbanders@stanford.edu",
     "description": "Species distribution modeling support tools",
-    "keywords": ["maxent", "biogeography", "SDM", "species distribution modeling", "ecologyy", "conservation"],
-    "packages": ["ccb"],
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "keywords": [
+        "maxent",
+        "biogeography",
+        "SDM",
+        "species distribution modeling",
+        "ecologyy",
+        "conservation",
+    ],
+    "packages": ["elapid"],
     "include_package_data": True,
     "platforms": "any",
     "scripts": ["bin/gbif-to-vector.py", "bin/vector-to-maxent.py"],
-    "data_files": [("maxent", ["ccb/maxent/maxent.jar", "ccb/maxent/README.txt", "ccb/maxent/LICENSE.txt"])],
+    "data_files": [
+        (
+            "maxent",
+            [
+                "elapid/maxent/maxent.jar",
+                "elapid/maxent/README.txt",
+                "elapid/maxent/LICENSE.txt",
+            ],
+        )
+    ],
+    "classifiers": [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 }
 
 setup(**setup_args)
