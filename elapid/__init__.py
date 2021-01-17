0d1acd31d7d49859032f3f52faad639cffeb816e
earth-chris <cba@salo.ai>
2021-01-16 17:45:54 -0800

    added logging, updated imports

--- elapid/__init__.py
+++ elapid/__init__.py
@@ -1,2 +1,13 @@
+"""Configuration for loading the elapid package"""
+import logging as _logging
+import sys as _sys
+
 from . import metrics, plot, read
-from ._core import *
+from .ml import sk_tuner
+from .utils import maxent, run
+
+_logging.basicConfig(
+    level=_logging.WARNING,
+    format=("%(asctime)s %(levelname)s %(name)s [%(funcName)s] | %(message)s"),
+    stream=_sys.stdout,
+)
